76b1f3bcbb1423faa30bad617fb968f302725e75
Andrew Kolev <alteraweb53@gmail.com>
2023-08-16 16:18:45 +0300

    add views

---
+++ src/Views/admin/layouts/master.blade.php
@@ -0,0 +1,3 @@
+<x-admin::layout>
+    @yield('panel')
+</x-admin::layout>
---
+++ src/Views/admin/setting/general.blade.php
@@ -0,0 +1,72 @@
+@php $general = gs(); @endphp
+@extends('admin::layouts.app')
+@section('panel')
+    <div class="row mb-none-30">
+        <div class="col-lg-12 col-md-12 mb-30">
+            <div class="card">
+                <div class="card-body">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-4 col-sm-6">
+                                <div class="form-group ">
+                                    <label> @lang('Site Title')</label>
+                                    <input class="form-control" type="text" name="site_name" required value="{{$general->site_name}}">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-3 col-sm-6">
+                                <label>@lang('Timezone')</label>
+                                <select class="select2-basic" name="timezone">
+                                    @foreach($timezones as $timezone)
+                                    <option value="'{{ @$timezone}}'">{{ __($timezone) }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn--primary w-100 h-45">@lang('Submit')</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('script-lib')
+    <script src="{{ asset('assets/admin/js/spectrum.js') }}"></script>
+@endpush
+
+@push('style-lib')
+    <link rel="stylesheet" href="{{ asset('assets/admin/css/spectrum.css') }}">
+@endpush
+
+@push('script')
+    <script>
+        (function ($) {
+            "use strict";
+            $('.colorPicker').spectrum({
+                color: $(this).data('color'),
+                change: function (color) {
+                    $(this).parent().siblings('.colorCode').val(color.toHexString().replace(/^#?/, ''));
+                }
+            });
+
+            $('.colorCode').on('input', function () {
+                var clr = $(this).val();
+                $(this).parents('.input-group').find('.colorPicker').spectrum({
+                    color: clr,
+                });
+            });
+
+            $('select[name=timezone]').val("'{{ config('app.timezone') }}'").select2();
+            $('.select2-basic').select2({
+                dropdownParent:$('.card-body')
+            });
+        })(jQuery);
+
+    </script>
+@endpush
+
