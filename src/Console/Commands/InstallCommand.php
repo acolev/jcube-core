6f4fd7e005b9d0358a9ff6a8425c42a5bff5daf5
Andrew Kolev <alteraweb53@gmail.com>
2023-08-22 11:21:18 +0300

    migration

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -6,70 +6,73 @@ use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
-    protected $name = 'jcube:install';
+	protected $name = 'jcube:install';
 
-    protected $description = 'Install all of the jCube files';
+	protected $description = 'Install all of the jCube files';
 
-    public function handle()
-    {
-        $metadata = json_decode(file_get_contents(dirname(__DIR__, 3).'/composer.json'));
+	public function handle()
+	{
+		$metadata = json_decode(file_get_contents(dirname(__DIR__, 3) . '/composer.json'));
 
-        $this->comment('Installation started. Please wait...');
-        $this->info('Version: '.$metadata->version);
-        $this
-            ->executeCommand('migrate')
-            ->executeCommand('storage:link')
-            ->executeCommand('vendor:publish', [
-                '--tag' => 'core-config',
-            ])
-            ->showMeLove();
+		$this->comment('Installation started. Please wait...');
+		$this->info('Version: ' . $metadata->version);
+		$this
+			->executeCommand('storage:link')
+			->executeCommand('vendor:publish', [
+				'--tag' => 'core-config',
+			]);
 
-        $this->info('Completed!');
-        $this->comment("To create a user, run 'artisan jcube:admin'");
-        $this->line("To start the embedded server, run 'artisan serve'");
-    }
+		if ($this->confirm('Would you like run migrate?')) {
+			$this->executeCommand('migrate');
+		}
+		$this->showMeLove();
 
-    /**
-     * @return $this
-     */
-    private function executeCommand(string $command, array $parameters = []): self
-    {
-        try {
-            $result = $this->callSilent($command, $parameters);
-        } catch (\Exception $exception) {
-            $result = 1;
-            $this->alert($exception->getMessage());
-        }
+		$this->info('Completed!');
+		$this->comment("To create a user, run 'artisan jcube:admin'");
+		$this->line("To start the embedded server, run 'artisan serve'");
+	}
 
-        if ($result) {
-            $parameters = http_build_query($parameters, '', ' ');
-            $parameters = str_replace('%5C', '/', $parameters);
-            $this->alert("An error has occurred. The '{$command} {$parameters}' command was not executed");
-        }
+	/**
+	 * @return $this
+	 */
+	private function executeCommand(string $command, array $parameters = []): self
+	{
+		try {
+			$result = $this->callSilent($command, $parameters);
+		} catch (\Exception $exception) {
+			$result = 1;
+			$this->alert($exception->getMessage());
+		}
 
-        return $this;
-    }
+		if ($result) {
+			$parameters = http_build_query($parameters, '', ' ');
+			$parameters = str_replace('%5C', '/', $parameters);
+			$this->alert("An error has occurred. The '{$command} {$parameters}' command was not executed");
+		}
 
-    /**
-     * @return $this
-     */
-    private function showMeLove(): self
-    {
-        if (! $this->confirm('Would you like to show a little love by starting with ⭐')) {
-            return $this;
-        }
+		return $this;
+	}
 
-        $repo = 'https://gitlab.com/jcubegroup/jcube-core-v2';
+	/**
+	 * @return $this
+	 */
+	private function showMeLove(): self
+	{
+		if (!$this->confirm('Would you like to show a little love by starting with ⭐')) {
+			return $this;
+		}
 
-        match (PHP_OS_FAMILY) {
-            'Darwin' => exec('open '.$repo),
-            'Windows' => exec('start '.$repo),
-            'Linux' => exec('xdg-open '.$repo),
-            default => $this->line('You can find us at '.$repo),
-        };
+		$repo = 'https://gitlab.com/jcubegroup/jcube-core-v2';
 
-        $this->line('Thank you! It means a lot to us! 🙏');
+		match (PHP_OS_FAMILY) {
+			'Darwin' => exec('open ' . $repo),
+			'Windows' => exec('start ' . $repo),
+			'Linux' => exec('xdg-open ' . $repo),
+			default => $this->line('You can find us at ' . $repo),
+		};
 
-        return $this;
-    }
+		$this->line('Thank you! It means a lot to us! 🙏');
+
+		return $this;
+	}
 }
