0e21a4e34fe6ec3c690cb8154df1b1b261c4d4a1
Andrew Kolev <alteraweb53@gmail.com>
2023-08-23 17:54:21 +0300

    fix css

--- src/Http/Controllers/Admin/GeneralSettingController.php
+++ src/Http/Controllers/Admin/GeneralSettingController.php
@@ -10,132 +10,135 @@ use jCube\Rules\FileTypeValidate;
 
 class GeneralSettingController extends Controller
 {
-    public function index()
-    {
-        $pageTitle = 'General Setting';
-        $timezones = timezoneList();
+	public function index()
+	{
+		$pageTitle = 'General Setting';
+		$timezones = timezoneList();
 
-        return view('admin::setting.general', compact('pageTitle', 'timezones'));
-    }
+		return view('admin::setting.general', compact('pageTitle', 'timezones'));
+	}
 
-    public function update(Request $request)
-    {
-        $request->validate([
-            'site_name' => 'required|string|max:40',
-        ]);
+	public function update(Request $request)
+	{
+		$request->validate([
+			'site_name' => 'required|string|max:40',
+		]);
 
-        $general = gs();
-        $general->site_name = $request->site_name;
-        $general->data_values = array_merge((array) $general->data_values, (array) $request->data_values);
-        $general->save();
+		$general = gs();
+		$general->site_name = $request->site_name;
+		$general->data_values = array_merge((array)$general->data_values, (array)$request->data_values);
+		$general->save();
 
-        $timezoneFile = config_path('timezone.php');
-        $content = '<?php $timezone = '.$request->timezone.' ?>';
-        file_put_contents($timezoneFile, $content);
-        Artisan::call('optimize:clear');
-        $notify[] = ['success', 'General setting updated successfully'];
+		$timezoneFile = config_path('timezone.php');
+		$content = '<?php $timezone = ' . $request->timezone . ' ?>';
+		file_put_contents($timezoneFile, $content);
+		Artisan::call('optimize:clear');
+		$notify[] = ['success', 'General setting updated successfully'];
 
-        return back()->withNotify($notify);
-    }
+		return back()->withNotify($notify);
+	}
 
-    public function systemConfiguration()
-    {
-        $pageTitle = 'System Configuration';
+	public function systemConfiguration()
+	{
+		$pageTitle = 'System Configuration';
 
-        return view('admin::setting.configuration', compact('pageTitle'));
-    }
+		return view('admin::setting.configuration', compact('pageTitle'));
+	}
 
-    public function systemConfigurationSubmit(Request $request)
-    {
-        $general = gs();
-        $general->data_values = array_merge((array) $general->data_values, (array) $request->data_values);
-        $general->save();
-        Artisan::call('optimize:clear');
-        $notify[] = ['success', 'System configuration updated successfully'];
+	public function systemConfigurationSubmit(Request $request)
+	{
+		$general = gs();
+		$general->data_values = array_merge((array)$general->data_values, (array)$request->data_values);
+		$general->save();
+		Artisan::call('optimize:clear');
+		$notify[] = ['success', 'System configuration updated successfully'];
 
-        return back()->withNotify($notify);
-    }
+		return back()->withNotify($notify);
+	}
 
-    public function logoIcon()
-    {
-        $pageTitle = 'Logo & Favicon';
+	public function logoIcon()
+	{
+		$pageTitle = 'Logo & Favicon';
 
-        return view('admin::setting.logo_icon', compact('pageTitle'));
-    }
+		return view('admin::setting.logo_icon', compact('pageTitle'));
+	}
 
-    public function logoIconUpdate(Request $request)
-    {
+	public function logoIconUpdate(Request $request)
+	{
 
-        $request->validate([
-            'logo' => ['image', new FileTypeValidate(['jpg', 'jpeg', 'png'])],
-            'logo_dark' => ['image', new FileTypeValidate(['jpg', 'jpeg', 'png'])],
-            'favicon' => ['image', new FileTypeValidate(['png'])],
-        ]);
+		$request->validate([
+			'logo' => ['image', new FileTypeValidate(['jpg', 'jpeg', 'png'])],
+			'logo_dark' => ['image', new FileTypeValidate(['jpg', 'jpeg', 'png'])],
+			'favicon' => ['image', new FileTypeValidate(['png'])],
+		]);
 
-        if ($request->hasFile('logo')) {
-            try {
-                $path = getFilePath('logoIcon');
-                if (! file_exists($path)) {
-                    mkdir($path, 0755, true);
-                }
-                Image::make($request->logo)->save($path.'/logo.png');
-            } catch (\Exception $exp) {
-                $notify[] = ['error', 'Couldn\'t upload the logo'];
+		if ($request->hasFile('logo')) {
+			try {
+				$path = getFilePath('logoIcon');
+				if (!file_exists($path)) {
+					mkdir($path, 0755, true);
+				}
+				Image::make($request->logo)->save($path . '/logo.png');
+			} catch (\Exception $exp) {
+				$notify[] = ['error', 'Couldn\'t upload the logo'];
 
-                return back()->withNotify($notify);
-            }
-        }
-        if ($request->hasFile('logo_dark')) {
-            try {
-                $path = getFilePath('logoIcon');
-                if (! file_exists($path)) {
-                    mkdir($path, 0755, true);
-                }
-                Image::make($request->logo_dark)->save($path.'/logo_dark.png');
-            } catch (\Exception $exp) {
-                $notify[] = ['error', 'Couldn\'t upload the logo'];
+				return back()->withNotify($notify);
+			}
+		}
+		if ($request->hasFile('logo_dark')) {
+			try {
+				$path = getFilePath('logoIcon');
+				if (!file_exists($path)) {
+					mkdir($path, 0755, true);
+				}
+				Image::make($request->logo_dark)->save($path . '/logo_dark.png');
+			} catch (\Exception $exp) {
+				$notify[] = ['error', 'Couldn\'t upload the logo'];
 
-                return back()->withNotify($notify);
-            }
-        }
+				return back()->withNotify($notify);
+			}
+		}
 
-        if ($request->hasFile('favicon')) {
-            try {
-                $path = getFilePath('logoIcon');
-                if (! file_exists($path)) {
-                    mkdir($path, 0755, true);
-                }
-                $size = explode('x', getFileSize('favicon'));
-                Image::make($request->favicon)->resize($size[0], $size[1])->save($path.'/favicon.png');
-            } catch (\Exception $exp) {
-                $notify[] = ['error', 'Couldn\'t upload the favicon'];
+		if ($request->hasFile('favicon')) {
+			try {
+				$path = getFilePath('logoIcon');
+				if (!file_exists($path)) {
+					mkdir($path, 0755, true);
+				}
+				$size = explode('x', getFileSize('favicon'));
+				Image::make($request->favicon)->resize($size[0], $size[1])->save($path . '/favicon.png');
+			} catch (\Exception $exp) {
+				$notify[] = ['error', 'Couldn\'t upload the favicon'];
 
-                return back()->withNotify($notify);
-            }
-        }
-        $notify[] = ['success', 'Logo & favicon updated successfully'];
+				return back()->withNotify($notify);
+			}
+		}
+		$notify[] = ['success', 'Logo & favicon updated successfully'];
 
-        return back()->withNotify($notify);
-    }
+		return back()->withNotify($notify);
+	}
 
-    public function customCss()
-    {
-        $pageTitle = 'Custom CSS';
-        $file = activeTemplate(true).'css/custom.css';
-        $fileContent = @file_get_contents($file);
+	public function customCss()
+	{
+		$pageTitle = 'Custom CSS';
+		$file = activeTemplate(true) . 'css/custom.css';
+		$fileContent = @file_get_contents($file);
 
-        return view('admin::setting.custom_css', compact('pageTitle', 'fileContent'));
-    }
+		return view('admin::setting.custom_css', compact('pageTitle', 'fileContent'));
+	}
 
-    public function customCssSubmit(Request $request)
-    {
-        $file = activeTemplate(true).'css/custom.css';
-        if (! file_exists($file)) {
-            fopen($file, 'w');
-        }
-        file_put_contents($file, $request->css);
-        $notify[] = ['success', 'CSS updated successfully'];
+	public function customCssSubmit(Request $request)
+	{
+		$file = activeTemplate(true) . 'css/custom.css';
+		if (!is_dir(dirname($file))) {
+			mkdir(dirname($file), 0755, true);
+		}
+		if (!file_exists($file)) {
+			fopen($file, 'w');
+		}
+		file_put_contents($file, $request->css);
+		$notify[] = ['success', 'CSS updated successfully'];
 
-        return back()->withNotify($notify);
-    }
+		return back()->withNotify($notify);
+	}
 }
