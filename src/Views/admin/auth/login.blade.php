31093c80fac8545cdee632f2e8ee275a5ba0d3ec
Andrew Kolev <alteraweb53@gmail.com>
2023-08-23 14:00:58 +0300

    fix login

--- src/Views/admin/auth/login.blade.php
+++ src/Views/admin/auth/login.blade.php
@@ -5,7 +5,7 @@
                 <div class="align-items-center d-flex h-100 justify-content-center">
                     <div class="w-75">
                         <h4 class="logo-text mb-3">@lang('Welcome to')
-                            <strong>{{__(gs()->site_name ?: 'jCube Admin')}}</strong></h4>
+                            <strong>{{__(gs()->site_name ?: env('APP_NAME'))}}</strong></h4>
                         <p>{{__($pageTitle)}} @lang('to')  {{__(gs()->site_name ?: env('APP_NAME'))}} @lang('dashboard')</p>
                         <form action="{{ route('admin.login') }}" method="POST" class="cmn-form mt-5">
                             @csrf
