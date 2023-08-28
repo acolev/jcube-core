d094438c6b6687dd1f84273d2560789340f55ae5
Andrew Kolev <alteraweb53@gmail.com>
2023-08-28 14:03:01 +0300

    update x2

---
+++ database/migrations/2023_08_28_105948_fill_config_categories.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use jCube\Models\ConfigCategory;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		$cat = new ConfigCategory();
+		$cat->slug = 'general';
+		$cat->title = 'General Settings';
+		$cat->descriptrion = 'General settings and configuration';
+		$cat->icon = 'la la-cog';
+		$cat->save();
+
+		$cat = new ConfigCategory();
+		$cat->slug = 'system';
+		$cat->title = 'System Configuration';
+		$cat->descriptrion = 'System settings and configuration';
+		$cat->icon = 'la la-database';
+		$cat->save();
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		//
+	}
+};
