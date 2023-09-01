5bafb27bc8bc7ddc3e5269676a9002ae78993c5c
Andrew Kolev <alteraweb53@gmail.com>
2023-09-01 15:31:24 +0300

    fix menu

--- src/Views/components/admin/menu/index.blade.php
+++ src/Views/components/admin/menu/index.blade.php
@@ -7,11 +7,11 @@
         @if((!isset($item["link"]['name']) || Route::has($item["link"]['name'])) && (!isset($item['access']) || @$admin?->access($item['access'])) )
             @if(isset($item['children']))
                 <li class="sidebar-menu-item {{ @$item['children'] ? 'sidebar-dropdown' : '' }}">
-                    <a href="javascript:void(0)" class="nav-link nav-parent-{{titleToKey($item['name'])}} {{ menuActive($item["link"]['active'], 3) }}">
+                    <a href="javascript:void(0)" class="nav-link nav-parent-{{titleToKey(@$item['name'])}} {{ menuActive(@$item["link"]['active'], 3) }}">
                         <i class="menu-icon {{ @$item['icon'] ?: 'la la-circle' }}"></i>
                         <span class="menu-title">{{ __($item['name']) }}</span>
                     </a>
-                    <div class="sidebar-submenu {{menuActive($item["link"]['active'],2)}} ">
+                    <div class="sidebar-submenu {{menuActive(@$item["link"]['active'],2)}} ">
                         <ul>
                             @foreach($item['children'] as $menu)
                                 @php $menu['icon'] = 'las la-dot-circle'; @endphp
@@ -22,7 +22,7 @@
                 </li>
             @else
                 <li class="sidebar-menu-item {{ menuActive($item["link"]['active']) }}">
-                    <a href="{{route($item["link"]['name'], @$item["link"]['params'])}}" class="nav-link nav-item-{{titleToKey($item['name'])}}"
+                    <a href="{{route($item["link"]['name'], @$item["link"]['params'])}}" class="nav-link nav-item-{{titleToKey(@$item['name'])}}"
                        @if(is_array(@$item['search']))
                            data-search-query="{{ @$item['search']['query'] }}"
                        data-search-title="{{ __(@$item['search']['title']) }}"
@@ -30,7 +30,7 @@
                        @endif
                        target="{{ @$item['target'] }}">
                         <i class="menu-icon {{ @$item['icon'] ?: 'la la-circle' }}"></i>
-                        <span class="menu-title">{{ __($item['name']) }}</span>
+                        <span class="menu-title">{{ __(@$item['name']) }}</span>
                     </a>
                 </li>
             @endif
