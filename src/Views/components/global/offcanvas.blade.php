83a18abaae0a3437d85124aac0e90d09f82b546e
Andrew Kolev <alteraweb53@gmail.com>
2023-09-06 11:28:12 +0300

    fix configs

--- src/Views/components/global/offcanvas.blade.php
+++ src/Views/components/global/offcanvas.blade.php
@@ -12,17 +12,16 @@
         <i class="la la-bars p-0"></i>
     </button>
 @endisset
-@push('modal-place')
-    <div class="offcanvas offcanvas-{{$pos}}" tabindex="-1" id="{{ $id }}" aria-labelledby="{{ $id }}Label"
-         @if($width) style="min-width: {{$width}}" @endif>
-        <div class="offcanvas-header">
-            @if($title)
-                <h5 class="offcanvas-title" id="{{ $id }}Label">{{ __($title) }}</h5>
-            @endif
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            {{ $slot }}
-        </div>
+
+<div class="offcanvas offcanvas-{{$pos}}" tabindex="-1" id="{{ $id }}" aria-labelledby="{{ $id }}Label"
+     @if($width) style="min-width: {{$width}}" @endif>
+    <div class="offcanvas-header">
+        @if($title)
+            <h5 class="offcanvas-title" id="{{ $id }}Label">{{ __($title) }}</h5>
+        @endif
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-@endpush
+    <div class="offcanvas-body">
+        {{ $slot }}
+    </div>
+</div>
