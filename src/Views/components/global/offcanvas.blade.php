2e593aaa90f0b558308463418b83de2b9d08eaab
Andrew Kolev <alteraweb53@gmail.com>
2023-09-06 11:11:50 +0300

    fix configs

--- src/Views/components/global/offcanvas.blade.php
+++ src/Views/components/global/offcanvas.blade.php
@@ -4,15 +4,24 @@
   'title' => '',
   'width' => null,
 ])
-<div class="offcanvas offcanvas-{{$pos}}" tabindex="-1" id="{{ $id }}" aria-labelledby="{{ $id }}Label"
-     @if($width) style="width: {{$width}}" @endif>
-    <div class="offcanvas-header">
-        @if($title)
-            <h5 class="offcanvas-title" id="{{ $id }}Label">{{ __($title) }}</h5>
-        @endif
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+@isset($button)
+{{$button}}
+@else
+    <button class="btn btn--primary">
+        <i class="la la-bars"></i>
+    </button>
+@endisset
+@push('modal-place')
+    <div class="offcanvas offcanvas-{{$pos}}" tabindex="-1" id="{{ $id }}" aria-labelledby="{{ $id }}Label"
+         @if($width) style="width: {{$width}}" @endif>
+        <div class="offcanvas-header">
+            @if($title)
+                <h5 class="offcanvas-title" id="{{ $id }}Label">{{ __($title) }}</h5>
+            @endif
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            {{ $slot }}
+        </div>
     </div>
-    <div class="offcanvas-body">
-        {{ $slot }}
-    </div>
-</div>
+@endpush
