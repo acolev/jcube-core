054b9dcf3c556585463e8d0d6a7fcb3bc4db2ad2
Andrew Kolev <alteraweb53@gmail.com>
2023-09-22 15:50:23 +0300

    update: editor

--- src/Views/components/global/form/html.blade.php
+++ src/Views/components/global/form/html.blade.php
@@ -23,8 +23,8 @@
           statusbar: false,
           menubar: {{ $menubar }},
             @if($type == 'full')
-            plugins: 'preview case importcss searchreplace autolink autosave directionality visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
-          toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor case removeformat help | pagebreak | charmap emoticons | fullscreen  preview | insertfile image media template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+            plugins: 'preview case importcss searchreplace autolink autosave directionality visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons code',
+          toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor case removeformat help | pagebreak | charmap emoticons | fullscreen  preview | insertfile image media template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment | code',
           quickbars_selection_toolbar: 'bold italic underline strikethrough | quicklink h2 h3 blockquote | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | case',
           templates: [
             {title: 'Article', description: '', content: '<h2>Header 2</h2> <p>story</p>'},
