1f781efafb248a4ecf4d963c27a69935dfb5a8c2
Andrew Kolev <alteraweb53@gmail.com>
2023-09-22 14:54:58 +0300

    update: editor

--- src/Views/components/global/form/html.blade.php
+++ src/Views/components/global/form/html.blade.php
@@ -22,15 +22,17 @@
           selector: '.tinymce',
           statusbar: false,
           menubar: {{ $menubar }},
-          @if($type == 'full')
-            plugins: 'table image lists autoresize code',
-            toolbar1: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | indent outdent | wordcount | image',
-            toolbar2: 'code',
-          @else
+            @if($type == 'full')
+            plugins: 'preview importcss searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons code',
+          toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor casechange permanentpen removeformat | pagebreak | charmap emoticons | fullscreen  preview | image media template link anchor codesample | showcomments addcomment code',
+          templates: [
+            {title: 'Article', description: '', content: '<h2>Header 2</h2> <p>story</p>'},
+          ],
+            @else
             plugins: 'lists autoresize',
-            toolbar: 'bold italic h2 h3 numlist bullist blockquote hr undo redo',
-          @endif
-          autoresize: 'on',
+          toolbar: 'bold italic h2 h3 numlist bullist blockquote hr undo redo',
+            @endif
+            autoresize: 'on',
           images_upload_url: '/api/images/editor',
         });
       })();
