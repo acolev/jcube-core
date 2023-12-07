e4b4cf317347a27a8f3b27a8ed95f028afc29157
Andrew Kolev <alteraweb53@gmail.com>
2023-12-07 17:43:44 +0200

    --

--- src/Views/components/global/tabs/index.blade.php
+++ src/Views/components/global/tabs/index.blade.php
@@ -1,22 +1,22 @@
 @php
-    $id = Illuminate\Support\Str::random(12);
+  $id = Illuminate\Support\Str::random(12);
 @endphp
 
-<div class="tabs">
-    <ul class="nav nav-tabs nav-primary" id="{{ $id }}" role="tablist">
-        @stack('tabs-buttons')
-        @if(isset($actions))
-            <li class="col"></li>
-            <li>
-                <div class="btn-group">
-                    {{ $actions }}
-                </div>
-            </li>
-        @endif
-    </ul>
-    <div class="content">
-        <div class="tab-content" id="{{ $id }}">
-            {{ $slot }}
+<div class="{{ @$btnWrapperCls }}">
+  <ul class="nav nav-tabs nav-primary {{ @$navCls }}" id="{{ $id }}" role="tablist" {{ $attributes }}>
+    @stack('tabs-buttons')
+    @if(isset($actions))
+      <li class="col"></li>
+      <li>
+        <div class="btn-group">
+          {{ $actions }}
         </div>
-    </div>
+      </li>
+    @endif
+  </ul>
+</div>
+<div class="content {{ @$contentCls }}">
+  <div class="tab-content" id="{{ $id }}">
+    {{ $slot }}
+  </div>
 </div>
