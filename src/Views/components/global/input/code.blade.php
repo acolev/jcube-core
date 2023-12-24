3d7617297dff5b8ac834629f68f3c53c976b7554
Andrew Kolev <alteraweb53@gmail.com>
2023-12-24 19:11:38 +0200

    --

--- src/Views/components/global/input/code.blade.php
+++ src/Views/components/global/input/code.blade.php
@@ -9,7 +9,6 @@
 	"inline" => false,
 	"variants" => [],
 	"id" => null,
-	"lang" => 'html',
 ])
 
 @if($label)
@@ -18,48 +17,48 @@
 <textarea class="codemirror" name="{{ $name }}" id="{{$id}}" {{ $attributes }}>@php echo $value @endphp</textarea>
 
 @pushonce('style-lib')
-    <link rel="stylesheet" href="{{asset('admin_assets/libs/codemirror/css/codemirror.min.css')}}">
-    <link rel="stylesheet" href="{{asset('admin_assets/libs/codemirror/css/monokai.min.css')}}">
+  <link rel="stylesheet" href="http://esironal.github.io/cmtouch/lib/codemirror.css">
+  <link rel="stylesheet" href="https://codemirror.net/5/theme/material.css">
 @endpushonce
 @pushonce('style')
-    <style>
-        .CodeMirror {
-            border-top: 1px solid #eee;
-            border-bottom: 1px solid #eee;
-            line-height: 1.3;
-            height: 500px;
-        }
-        .CodeMirror-linenumbers {
-            padding: 0 8px;
-        }
-        .custom-css p, .custom-css li, .custom-css span {
-            color: white;
-        }
-        .cm-s-monokai span.cm-tag {
-            margin-left: 15px;
-        }
-    </style>
+  <style>
+      .CodeMirror {
+          border-top: 1px solid #eee;
+          border-bottom: 1px solid #eee;
+          line-height: 1.3;
+          height: 500px;
+      }
+
+      .CodeMirror-linenumbers {
+          padding: 0 8px;
+      }
+
+      .custom-css p, .custom-css li, .custom-css span {
+          color: white;
+      }
+
+      .cm-s-monokai span.cm-tag {
+          margin-left: 15px;
+      }
+  </style>
 @endpushonce
 
 @pushonce('script-lib')
-    <script src="{{asset('admin_assets/libs/codemirror/js/codemirror.min.js')}}"></script>
-    <script src="{{asset('admin_assets/libs/codemirror/js/css.min.js')}}"></script>
-    <script src="{{asset('admin_assets/libs/codemirror/js/sublime.min.js')}}"></script>
+  <script src="http://esironal.github.io/cmtouch/lib/codemirror.js"></script>
+  <script src="http://esironal.github.io/cmtouch/mode/css/css.js"></script>
+  <script src="http://esironal.github.io/cmtouch/mode/xml/xml.js"></script>
 @endpushonce
 
 @pushonce('script')
-    <script>
-      "use strict";
-      document.querySelectorAll(".codemirror").forEach(function (el) {
-        var editor = CodeMirror.fromTextArea(el, {
-          lineNumbers: true,
-          mode: "text/{{ $lang }}",
-          theme: "monokai",
-          keyMap: "sublime",
-          autoCloseBrackets: true,
-          matchBrackets: true,
-          showCursorWhenSelecting: true,
-        });
-      })
-    </script>
+  <script>
+    document.querySelectorAll(".codemirror").forEach(function (el) {
+      CodeMirror.fromTextArea(el, {
+        lineNumbers: true,
+        theme: 'material',
+        autoCloseBrackets: true,
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
+      });
+    });
+  </script>
 @endpushonce
