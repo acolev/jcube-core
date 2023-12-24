a4fdb752c96abcd3e82ff9831fad3d44b84fed81
Andrew Kolev <alteraweb53@gmail.com>
2023-12-24 21:51:04 +0200

    --

--- src/Views/components/php/columns/item.blade.php
+++ src/Views/components/php/columns/item.blade.php
@@ -3,16 +3,16 @@
     {{ $slot }}
   </div>
 @else
-  @php $is_active =   titleToKey($active) === titleToKey($name)@endphp
+  @php $is_active =   slug($active) === slug($name) @endphp
   @push($columns)
     <li class="nav-item @if($is_active) active @endif" role="presentation">
       <button class="nav-link @if($is_active) active @endif" id="{{ $name }}-tab" data-bs-toggle="tab"
-              data-bs-target="#{{ titleToKey($name) }}" type="button"
+              data-bs-target="#{{ slug($name) }}" type="button"
               role="tab" aria-controls="{{ $name }}"
               aria-selected="{{ $is_active ? 'true' : 'false' }}">{{ __($name) }}</button>
     </li>
   @endpush
-  <div class="tab-pane fade @if($is_active) show active @endif" id="{{ titleToKey($name) }}" role="tabpanel"
+  <div class="tab-pane fade @if($is_active) show active @endif" id="{{ slug($name) }}" role="tabpanel"
        aria-labelledby="{{ $name }}-tab">
     {{ $slot }}
   </div>
