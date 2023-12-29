0883806e52a12e5522af98527e35663a6622dc97
Andrew Kolev <alteraweb53@gmail.com>
2023-12-29 12:41:44 +0200

    --

--- src/Views/components/php/table/item.blade.php
+++ src/Views/components/php/table/item.blade.php
@@ -1,12 +1,11 @@
-
 @if(@$fields)
   <tr {{ $attributes }}>
-    @foreach($fields as $field)
-        @if(isset(${"cell_" . $field}))
+    @foreach($fields as $key=>$field)
+      @if(isset(${"cell_" . $field}))
         <td {{ ${"cell_" . $field}->attributes }}>{{ ${"cell_" . $field} }}</td>
-        @else
-          <td>@php echo @$cols->$field @endphp</td>
-        @endif
+      @else
+        <td>@php echo @$cols->$field ?:  @$cols->$key @endphp</td>
+      @endif
     @endforeach
   </tr>
 @endif
