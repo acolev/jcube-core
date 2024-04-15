6e5c2de59b2ae9d87abbd6baaf39021f3f77afc8
Andrew Kolev <alteraweb53@gmail.com>
2024-04-15 09:06:18 +0300

    update modules

--- src/Console/Commands/ModuleCommand.php
+++ src/Console/Commands/ModuleCommand.php
@@ -3,80 +3,97 @@
 namespace jCube\Console\Commands;
 
 use Illuminate\Console\Command;
-use function Laravel\Prompts\error;
-use function Laravel\Prompts\intro;
+use Symfony\Component\Process\Process;
 
 class ModuleCommand extends Command
 {
-  protected $name = 'jcube:module';
   protected $signature = 'jcube:module {action} {name?}';
   protected $description = 'Install and update modules';
-  
   
   public function handle()
   {
     $action = $this->argument('action');
     $name = $this->argument('name');
-    $last_name = last(explode('/', $name));
-    $folder = ucfirst($last_name);
+    $folder = ucfirst(basename($name));
+    
     switch ($action) {
-      case "i":
       case "install":
-        intro('Installing: ' . $name);
-        exec('cd ./Modules && git clone ' . $this->genUrl($name) . ' ' . $folder, $output);
-        if (is_dir('./Modules/' . $folder)) {
-          \Artisan::call("module:enable {$folder}");
-          $this->info(implode(' ', $output));
-        } else {
-          $this->info(implode(' ', ['Sorry I can\'t install the', $folder]));
-        }
-        exec('composer du');
+      case "i":
+        $this->installModule($name, $folder);
         break;
-      case "u":
       case "update":
-        if (!$name) {
-          $items = glob('Modules/*');
-          if (is_array($items) && count($items)) {
-            foreach ($items as $item) {
-              $__name = explode('/', $item);
-              unset($__name[0]);
-              $__name = implode('/', $__name);
-              intro('Updating: ' . $__name);
-              exec('cd ./' . $item . ' && git pull', $output);
-              $this->info(implode(' ', $output));
-            }
-          } else {
-            error("Sorry I can't find any modules");
-          }
-          return '';
-        }
-        intro('Updating: ' . $name);
-        exec('cd ./Modules/' . $name . ' && git pull', $output);
-        $this->info(implode(' ', $output));
+      case "u":
+        $this->updateModule($name);
         break;
-      case "d":
       case "disable":
-        \Artisan::call("module:disable {$folder}");
+      case "d":
+        $this->disableModule($folder);
         break;
-      case "e":
       case "enable":
-        \Artisan::call("module:enable {$folder}");
+      case "e":
+        $this->enableModule($folder);
         break;
       default:
-        error("Sorry I don't know command: " . $action);
+        $this->error("Unknown command: " . $action);
         break;
     }
   }
   
+  private function installModule($name, $folder)
+  {
+    $this->info('Installing: ' . $name);
+    $process = new Process(['git', 'clone', $this->genUrl($name), "./Modules/$folder"]);
+    $process->setWorkingDirectory(base_path());
+    $process->run();
+    
+    if ($process->isSuccessful() && is_dir("./Modules/$folder")) {
+      \Artisan::call("module:enable $folder");
+      $this->info($process->getOutput());
+    } else {
+      $this->error("Failed to install $folder");
+    }
+  }
+  
+  private function updateModule($name)
+  {
+    if (!$name) {
+      $modules = glob(base_path('Modules/*'), GLOB_ONLYDIR);
+      foreach ($modules as $module) {
+        $this->updateModule(basename($module));
+      }
+      return;
+    }
+    
+    $this->info('Updating: ' . $name);
+    $process = new Process(['git', 'pull'], base_path("Modules/$name"));
+    $process->run();
+    
+    if (!$process->isSuccessful()) {
+      $this->error("Failed to update $name: " . $process->getErrorOutput());
+    } else {
+      $this->info($process->getOutput());
+    }
+  }
+  
+  private function disableModule($folder)
+  {
+    \Artisan::call("module:disable $folder");
+  }
+  
+  private function enableModule($folder)
+  {
+    \Artisan::call("module:enable $folder");
+  }
+  
   private function genUrl($name)
   {
-    return implode([
-      'https://',
-      $_ENV['MODULES_CREDENTIALS'],
-      '@',
-      str_replace('https://', '', $_ENV['MODULES_REPOSITORY']),
-      '/',
-      $name . '.git',
-    ]);
+    return "https://{$this->env('MODULES_CREDENTIALS')}@" .
+      str_replace('https://', '', $this->env('MODULES_REPOSITORY')) .
+      "/$name.git";
+  }
+  
+  private function env($key)
+  {
+    return env($key, '');
   }
 }
